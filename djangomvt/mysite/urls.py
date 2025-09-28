a4b88d3ed27f72131cf189d5b7db50c06d57045d
jacobstxt <153942144+jacobstxt@users.noreply.github.com>
2025-09-28 21:25:47 +0300

    feat: add product  listing and creation

--- djangomvt/mysite/urls.py
+++ djangomvt/mysite/urls.py
@@ -21,6 +21,7 @@ from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
+    path('products/', include('products.urls')),
     path('users/', include('users.urls')),
     path('categories/', include('categories.urls')),
     path('admin/', admin.site.urls),
