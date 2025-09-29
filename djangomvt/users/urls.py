b8dd91c517ed2a1d534b592bc0587207894c2484
jacobstxt <153942144+jacobstxt@users.noreply.github.com>
2025-09-29 20:41:22 +0300

    add login page

--- djangomvt/users/urls.py
+++ djangomvt/users/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name='users'
 
 urlpatterns = [
-    path('register/', views.register, name='register')
+    path('register/', views.register, name='register'),
+    path('login/', views.login, name='login')
 ]
