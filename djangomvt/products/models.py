7575aed315304edcd8563544909c8cd6e7fe3681
jacobstxt <153942144+jacobstxt@users.noreply.github.com>
2025-09-30 19:22:14 +0300

    feat:  product add is working

--- djangomvt/products/models.py
+++ djangomvt/products/models.py
@@ -1,7 +1,3 @@
-import uuid
-from io import BytesIO
-from django.core.files.base import ContentFile
-from PIL import Image
 from django.db import models
 from django.utils.text import slugify
 from unidecode import unidecode
@@ -37,7 +33,7 @@ class Product(models.Model):
 
 class ProductImage(models.Model):
     product = models.ForeignKey(
-        Product, on_delete=models.CASCADE, related_name="images"
+        Product, on_delete=models.CASCADE, related_name="images",null=True,blank=True
     )
     image = models.ImageField(upload_to="images/")
     priority = models.PositiveIntegerField(default=0, help_text="0 = головне фото")
@@ -47,17 +43,6 @@ class ProductImage(models.Model):
         ordering = ["priority", "created_at"]  # головні фото першими
 
     def save(self, *args, **kwargs):
-        if self.image:
-            img = Image.open(self.image)
-            if img.mode in ("RGBA", "P"):
-                img = img.convert("RGB")
-
-            filename = f"{uuid.uuid4().hex}.webp"
-            buffer = BytesIO()
-            img.save(buffer, format="WEBP")
-            buffer.seek(0)
-            self.image.save(filename, ContentFile(buffer.read()), save=False)
-
         super().save(*args, **kwargs)
 
     def __str__(self):
